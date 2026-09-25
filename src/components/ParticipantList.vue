badb7a27e47a1a6d2ed6cf1c0805ccad1e868e82
Thanathat Lumsan <thanagithub@email.com>
2026-09-25 11:17:04 +0700

    Add: answer to assignment-1

--- src/components/ParticipantList.vue
+++ src/components/ParticipantList.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref } from "vue";
 // 1. ประกาศตัวแปร name มีค่าเริ่มต้นเป็น ""
+const name = ref("");
 
 // 2. ประกาศตัวแปร participants เป็น Array เปล่าสำหรับเก็บค่า input name
+const participants = ref([]);
 
 // 3. สร้างฟังก์ชันสำหรับเพิ่ม name เข้า participants ตอนที่กดปุ่ม
 const addParticipant = () => {
-  // เริ่มเขียนโค้ดตรงนี้
+  participants.value.push(name.value);
+  name.value = "";
 };
 </script>
 
@@ -15,17 +18,17 @@ export default
   <div class="assignment-container">
     <h2>รายชื่อผู้เข้าร่วมกิจกรรม</h2>
     <!-- 4. ใช้ v-model เพื่อเก็บค่า input -->
-    <input placeholder="กรอกชื่อผู้เข้าร่วม" />
+    <input v-model="name" placeholder="กรอกชื่อผู้เข้าร่วม" />
     <!-- 5. ใช้ @click เพื่อเรียกใช้ addParticipant ตอนกดปุ่ม -->
-    <button>เพิ่มชื่อ</button>
+    <button @click="addParticipant">เพิ่มชื่อ</button>
 
     <div class="participant-list">
       <!-- 6. เขียน v-if เพื่อแสดงข้อความ "ยังไม่มีผู้เข้าร่วม" เมื่อไม่มีสมาชิกใน participants -->
-      <p>ยังไม่มีผู้เข้าร่วม</p>
+      <p v-if="participants.length === 0">ยังไม่มีผู้เข้าร่วม</p>
       <!-- 7. เขียน v-else เพื่อแสดง <li> ถ้ามีสมาชิกใน participants -->
-      <ul>
+      <ul v-else>
         <!-- 8. เขียน v-for เพื่อลูปและแสดงรายชื่อทั้งหมดใน participants -->
-        <li></li>
+        <li v-for="participant in participants" :key="participant">{{ participant }}</li>
       </ul>
     </div>
   </div>
